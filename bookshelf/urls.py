7cb3b5b107a54c08a1e9162551e5fbd7860fafff
Sergey Ilin <ilin_sergey90@mail.ru>
2024-02-10 22:09:20 +0300

    book datail view

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from bookshelf.views import library_view
+from bookshelf.views import book_detail_view, library_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', library_view),
+    path('books/<int:book_id>/', book_detail_view),
 ]
--- bookshelf/views.py
+++ bookshelf/views.py
@@ -7,3 +7,8 @@ from books.models import Book
 def library_view(request: HttpRequest) -> HttpResponse:
     books = Book.objects.all()
     return render(request, 'bookshelf/library.html', context={'books': books})
+
+
+def book_detail_view(request: HttpRequest, book_id: int) -> HttpResponse:
+    book = Book.objects.get(id=book_id)
+    return render(request, 'bookshelf/book_detail.html', context={'book': book})
